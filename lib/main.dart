a33df72f043824b33f7fc36596fa9098e0f366b1
mppmi <65011212117@msu.ac.th>
2024-10-14 17:15:56 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,10 @@ import 'package:mini_project_rider/page/login.dart';
 import 'package:mini_project_rider/page/page_Rider/OrderPageRider.dart';
 import 'package:mini_project_rider/page/page_User/Order.dart';
 import 'package:mini_project_rider/page/page_User/ProfilePage.dart';
+import 'package:mini_project_rider/page/page_User/Search.dart';
 import 'package:mini_project_rider/page/page_User/Status.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mini_project_rider/page/register.dart';
 import 'firebase_options.dart';
 
 
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const RegisterPage(),
     );
   }
 }
