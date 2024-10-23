847ed3b6823901f50f81868006b39f6442eb1041
mppmi <65011212117@msu.ac.th>
2024-10-23 21:23:21 +0700

    REnder

--- lib/config/internet_config.dart
+++ lib/config/internet_config.dart
@@ -1,2 +1,3 @@
 // String API_ENDPOINT = "http://10.10.31.66:3000";
-String API_ENDPOINT = "http://192.168.0.121:3000";
+// String API_ENDPOINT = "http://192.168.0.121:3000";
+String API_ENDPOINT = "https://projectrider-ykaj.onrender.com/";
